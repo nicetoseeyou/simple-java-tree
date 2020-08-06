817647832129f8ac67f6737e859cebb3139cfa61
nicetoseeyou <hello4world@yeah.net>
2020-08-06 22:45:27 +0800

    initial

---
+++ src/test/java/lab/nice/tree/AppTest.java
@@ -0,0 +1,18 @@
+package lab.nice.tree;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
